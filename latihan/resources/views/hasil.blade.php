8e20cc1c88e87ebcfda2ce1f0c5f243c7ef76bf4
jawa <syafiraazzahra_2226240082@mhs.mdp.ac.id>
2025-04-29 15:11:20 +0700

    belajar view route dan controller

---
+++ latihan/resources/views/hasil.blade.php
@@ -0,0 +1,13 @@
+<div>
+    Hasil Penjumlahan:
+    <br /> <?php echo $angka1;
+    echo '+';
+    echo $angka2;
+    echo '+';
+    echo $angka3;
+    echo '=';
+    echo $jumlah;
+    ?>
+
+
+</div>
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get("/berita/{id}/{title?}", function ($id,$title = NULL) {
+    return view('berita',['id' => $id, 'title' => $title]);
+});
+
+Route::get("/hasil/{jumlah}/{angka1}/{angka2}/{angka3?}", function ($angka1, $angka2, $angka3) {
+    return view('hasil',['jumlah' => $angka1 + $angka2 + $angka3,'angka1' => $angka1,'angka2' => $angka2, 'angka3' => $angka3]);
 });
