342cbe4c33e6c290e7db8e0a3bb0c0438c88706d
JustynCannavaro09 <cranelzeus1@gmail.com>
2025-04-29 17:52:03 +0700

    view

--- latihan/resources/views/berita.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h4>Ini Adalah Halaman Berita</h4>
-    ID: <?php echo $id; ?> <br/>
-    Title: <?php echo $title; ?><br/>
-</div>
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,15 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/profil', function () {
-    return view('profil');
-});
-
-Route::get("/berita/{id}/{title?}", function ($id,$title = NULL) {
-    return view('berita',['id' => $id, 'title' => $title]);
-});
-
-Route::get("/hasil/{jumlah}/{angka1}/{angka2}/{angka3?}", function ($angka1, $angka2, $angka3) {
-    return view('hasil',['jumlah' => $angka1 + $angka2 + $angka3,'angka1' => $angka1,'angka2' => $angka2, 'angka3' => $angka3]);
-});
