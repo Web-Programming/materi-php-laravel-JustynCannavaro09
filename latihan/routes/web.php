2d31ffe7faf729433c222f414be587bb21fce610
GoDisCover <95121614+GoDisCover@users.noreply.github.com>
2025-05-05 15:06:32 +0700

    controller

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/materi/index',[MateriController::class, 'index'] );
+
+Route::get('/materi/detail',[MateriController::class, 'detail'] );
