e6c226d0ce398f9e844fa3957c86d681c8a4f374
fery <ferysetiawan_2226240137@mhs.mdp.ac.id>
2025-05-27 14:59:22 +0700

    update latihan

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -1,49 +1,77 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\MateriController;
-use App\Http\Controllers\MhsApiController;
+use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserDosenController;
+use App\Http\Controllers\UserMahasiswaController;
+use GuzzleHttp\Middleware;
 use App\Http\Middleware\CekLogin;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\RouteAction;
 
-Route::get('/', [AuthController::class, 'login']);
-
-Route::get("/profil", function(){
-    return view("profil");
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get("/berita/{id}/{title?}", function($id, $title = NULL){
-    return view("berita", ['id' => $id, 'title' => $title]);
+Route::get('/about', function () {
+    return view('about');
 });
 
-Route::get("/total/{bil1}/{bil2?}/{bil3?}", 
-    function($bil1, $bil2, $bil3 = 0){
-    return view("hasil", [
-        'total' => $bil1 + $bil2 + $bil3, 
-        'bil1' => $bil1, 
-        'bil2' => $bil2, 
-        'bil3' => $bil3
-    ]);
+Route::get('/profil', function () {
+    return view('profil');
 });
-Route::resource('materi', MateriController::class);
+
+Route::get("/berita/{id}/{title?}", function ($id, $title = NULL) {
+    return view('berita', ['id' => $id, 'title' => $title]);
+});
+
+Route::get('/total/{angka1?}/{angka2?}/{angka3?}', function ($angka1 = 0, $angka2 = 0, $angka3 = 0) {
+    return view('hasil', ['angka1' => $angka1, 'angka2' => $angka2, 'angka3' => $angka3]);
+});
+
+Route::get('/fakultas', function () {
+    // return view('fakultas.index', ["ilkom" => "Fakultas Ilmu Komputer dan Rekayasa"]);
+    // return view('fakultas.index', ["fakultas" => ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"]]);
+    // return view('fakultas.index')->with("fakultas", ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"]);
+
+    $kampus = "Universitas Multi Data Palembang";
+    // $fakultas = [];
+    $fakultas = ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"];
+
+    return view('fakultasContoh.index', compact('fakultas', 'kampus'));
+});
+
+Route::get('/materi/index', [MateriController::class,'index']);
+
+# Route::get('/materi/detail/{$id}', [MateriControllerContoh::class, 'detail']);
+
+/*
+Route::resource('prodi', ProdiControllerContoh::class);
+
+Route::apiResource('api/mhs', MhsApiController::class);
+*/
+
+// Latihan Layout
 Route::resource('prodi', ProdiController::class);
-Route::resource('fakultas', FakultasController::class);
-Route::resource('mhs', MahasiswaController::class);
+Route::resource('materi', MateriController::class);
 Route::resource('dosen', DosenController::class);
+Route::resource('mhs', MahasiswaController::class);
+Route::resource('fakultas', FakultasController::class);
 
 Route::get('/master', function(){
-    return view('laravel.master');
+    return view('latihanLayout.master');
 });
 
 Route::get('/master', function(){
-    return view('laravel.masterisi');
+    return view('latihanLayout.masterisi');
 });
 
-//Authentication
 Route::get("/login", [AuthController::class, 'login'])->name('login');
 Route::post("/login", [AuthController::class, 'do_login']);
 Route::get("/register", [AuthController::class, 'register']);
@@ -54,10 +82,26 @@ Route::group(['middleware' => ['auth']], function(){
     Route::group(['middleware' => [CekLogin::class.':admin']], function(){
         Route::get("/admin", [AdminController::class, 'index']);
         Route::resource('prodi', ProdiController::class);
+        Route::resource('materi', MateriController::class);
+        Route::resource('dosen', DosenController::class);
+        Route::resource('mhs', MahasiswaController::class);
         Route::resource('fakultas', FakultasController::class);
     });
 
     Route::group(['middleware' => [CekLogin::class.':user']], function(){
         Route::get("/user", [UserController::class, 'index']);
     });
-});
+    
+    Route::group(['middleware' => [CekLogin::class.':dosen']], function(){
+        Route::get("/userdosen", [UserDosenController::class, 'index']);
+        /*
+        Route::resource('materi', MateriController::class);
+        Route::resource('mhs', MahasiswaController::class); */
+    });
+
+    Route::group(['middleware' => [CekLogin::class.':mahasiswa']], function(){
+        Route::get("/usermahasiswa", [UserMahasiswaController::class, 'index']);
+        /*
+        Route::resource('materi', MateriController::class); */
+    });
+}); 
